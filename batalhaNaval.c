724402a07ee6afe2ea8e5ca579d477f87bd80add
Matheus Magalhães <magalhaes.almeida.matheus@gmail.com>
2025-02-16 12:49:18 -0300

    feito sistema de ataque

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,144 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <stdlib.h>
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+char alpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int cone_attack(int water[10][10], int X, int Y)
+{
+    for (int L = 0; L < 10; L++)
+    {
+        for (int C = 0; C < 10; C++)
+        {
+            if (L == X - 1)
+            {
+                if (C == Y)
+                {
+                    water[L][C] = 1;
+                }
+            }
+            if (L == X)
+            {
+                if (C == Y - 1 || C == Y || C == Y + 1)
+                {
+                    water[L][C] = 1;
+                }
+            }
+            if (L == X + 1)
+            {
+                if (C == Y - 1 || C == Y - 2 || C == Y || C == Y + 1 || C == Y + 2)
+                {
+                    water[L][C] = 1;
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+int octa_attack(int water[10][10], int X, int Y)
+{
+    for (int L = 0; L < 10; L++)
+    {
+        for (int C = 0; C < 10; C++)
+        {
+            if (L == X - 1)
+            {
+                if (C == Y)
+                {
+                    water[L][C] = 1;
+                }
+            }
+            if (L == X)
+            {
+                if (C == Y - 1 || C == Y || C == Y + 1)
+                {
+                    water[L][C] = 1;
+                }
+            }
+            if (L == X + 1)
+            {
+                if (C == Y)
+                {
+                    water[L][C] = 1;
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+int acros_attack(int water[10][10], int X, int Y)
+{
+    for (int L = 0; L < 10; L++)
+    {
+        for (int C = 0; C < 10; C++)
+        {
+            if (L == X - 1)
+            {
+                if (C == Y)
+                {
+                    water[L][C] = 1;
+                }
+            }
+            if (L == X)
+            {
+                if (C == Y - 1 || C == Y - 2 || C == Y || C == Y + 1 || C == Y + 2)
+                {
+                    water[L][C] = 1;
+                }
+            }
+            if (L == X + 1)
+            {
+                if (C == Y)
+                {
+                    water[L][C] = 1;
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+int char_to_int(char word)
+{
+    int index;
+    for (int i = 0; i < 26; i++)
+    {
+        if (alpha[i] == word)
+        {
+            index = i;
+        }
+    }
+    return index;
+}
+
+int show(int water[10][10])
+{
+    system("clear");
+    printf("  ");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%c ", alpha[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%c ", alpha[i]);
+        for (int j = 0; j < 10; j++)
+        {
+            if (!water[i][j])
+                water[i][j] = 0;
+            printf("%d ", water[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
@@ -25,7 +159,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
@@ -35,6 +169,42 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+
+    int attack;
+    int water[10][10] = {0};
+    do
+    {
+        show(water);
+        printf("Escolha seu ataque: ");
+        printf("1 - Cone, 2 - Octa, 3 - Cruz, 9 - SAIR\n");
+        scanf("%d", &attack);
+
+        char X = 'A';
+        char Y = 'B';
+        printf("Onde atacar?\n");
+        printf("Linha: ");
+        scanf(" %c", &X);
+        printf("Coluna: ");
+        scanf(" %c", &Y);
+
+        printf("\n\n");
+
+        switch (attack)
+        {
+        case 1:
+            cone_attack(water, char_to_int(X), char_to_int(Y));
+            break;
+        case 2:
+            octa_attack(water, char_to_int(X), char_to_int(Y));
+            break;
+        case 3:
+            acros_attack(water, char_to_int(X), char_to_int(Y));
+            break;
+
+        default:
+            break;
+        }
+    } while (attack != 9);
 
     return 0;
 }
